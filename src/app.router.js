84c2e700f4c7cfbabb6ef8194a533a92e5089b3f
FatimaOmair <fatimaemer2002@gmail.com>
2024-04-29 09:00:07 +0300

    add cors

--- src/app.router.js
+++ src/app.router.js
@@ -1,14 +1,15 @@
 import connectDB from '../db/connection.js'
 import categoriesRouter from './modules/category/category.router.js'
 import productsRouter from './modules/product/product.router.js'
-
+import cors from 'cors'
 const initApp=(app,express)=>{
     connectDB()
+    app.use(cors())
      app.use(express.json())
      app.get('/',(req,res)=>{
         return res.status(200).json({message:"success"})
      })
-    app.use(categoriesRouter)
+    app.use('/categories',categoriesRouter)
     app.use(productsRouter)
 
     app.use('*',(req,res)=>{
