8452dddeb08771fc2cbef29714bf9293af3ec32c
FatimaOmair <fatimaemer2002@gmail.com>
2024-04-29 09:01:11 +0300

    create cloudinary file

---
+++ src/ults/cloudinary.js
@@ -0,0 +1,10 @@
+import 'dotenv/config'
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name:process.env.cloud_name , 
+  api_key:process.env.api_key , 
+  api_secret:process.env.api_secret  
+});
+
+export default cloudinary ;
