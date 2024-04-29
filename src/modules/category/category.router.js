92d8f254178d780495184806aedd54b50a54b736
FatimaOmair <fatimaemer2002@gmail.com>
2024-04-29 09:00:53 +0300

    add router for create category

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
 import * as categoryController from './category.controller.js'
+import fileUpload, { fileType } from '../../ults/multer.js';
 const router= Router();
 
-
-router.get('/categories',categoryController.getAll)
+router.post('/',fileUpload(fileType.image).single('image'), categoryController.create);
+router.get('/',categoryController.getAll)
 
 export default router;
