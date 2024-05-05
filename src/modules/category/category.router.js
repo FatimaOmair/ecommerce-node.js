056f574a717409928335132c73851c6ec99b021f
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-05 11:38:37 +0300

    add routerfs in category router

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -1,9 +1,16 @@
 import {Router} from 'express'
 import * as categoryController from './category.controller.js'
 import fileUpload, { fileType } from '../../ults/multer.js';
+import { auth } from '../../middleware/auth.js';
 const router= Router();
 
-router.post('/',fileUpload(fileType.image).single('image'), categoryController.create);
+router.post('/',auth(),fileUpload(fileType.image).single('image'), categoryController.create);
 router.get('/',categoryController.getAll)
+router.get('/active',categoryController.getActive)
+router.get('/:id',categoryController.getCategory)
+router.patch('/:id',auth(),fileUpload(fileType.image).single('image'),categoryController.updateCategory)
+router.delete('/:id',categoryController.deleteCategory)
+
+router.get('/name/:name',categoryController.getName)
 
 export default router;
