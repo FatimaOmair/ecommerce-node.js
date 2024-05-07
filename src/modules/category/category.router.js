f4736b6af0e264c586e7ffca99ed9d9887a01723
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-07 10:44:30 +0300

    add router .use in category router

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -2,10 +2,11 @@ import {Router} from 'express'
 import * as categoryController from './category.controller.js'
 import fileUpload, { fileType } from '../../ults/multer.js';
 import { auth } from '../../middleware/auth.js';
+import subCategoryRouter from './../subCategory/subCategory.router.js'
 const router= Router();
-
+router.use('/:id/subCategory',subCategoryRouter)
 router.post('/',auth(),fileUpload(fileType.image).single('image'), categoryController.create);
-router.get('/',categoryController.getAll)
+router.get('/',auth(),categoryController.getAll)
 router.get('/active',categoryController.getActive)
 router.get('/:id',categoryController.getCategory)
 router.patch('/:id',auth(),fileUpload(fileType.image).single('image'),categoryController.updateCategory)
