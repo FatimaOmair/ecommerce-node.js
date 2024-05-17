d49e85cc75a6041ddff5c1553f46a612f0349e93
FatimaOmair <fatimaemer2002@gmail.com>
2024-05-17 20:21:34 +0300

    create cart model

---
+++ db/model/cart.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema, Types, model } from "mongoose";
+
+const cartSchema = new Schema({
+    userId: {
+        type: Types.ObjectId,
+        ref: 'User',
+        required: true,
+        unique: true
+
+    },
+
+    products: [{
+        productId: {
+            type: Types.ObjectId,
+            ref: 'Product',
+            required: true
+        },
+
+        quantity: {
+            type: Number,
+            default: 1
+
+        },
+    }]
+
+
+
+
+}, {
+    timestamps: true,
+
+},
+)
+
+
+const cartModel = model('Cart', cartSchema)
+export default cartModel;
