2d88652434a0b82b349a53e2c477624d724e064a
FatimaOmair <fatimaemer2002@gmail.com>
2024-06-03 10:59:49 +0300

    create coupon model

--- db/model/coupon.model.js
+++ db/model/coupon.model.js
@@ -1,42 +1,27 @@
 import mongoose, { Schema, Types, model } from "mongoose";
 
-const couponSchema =new Schema({
-   name:{
-    type:String,
-    required:true,
-    unique:true
-    
-   },
-
-   amount:{
-    type:Number,
-    required:true
-  },
-
-   usedBy:[{
-    userId:{
-        type:Types.ObjectId,
-        ref:'User',
-        required:true
-
+const couponSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    usedBy: [{
+        type: Types.ObjectId,
+        ref: 'User',
+        default: []
+    }],
+    expireDate: {
+        type: Date,
+        required: true
     }
-   }],
+}, {
+    timestamps: true,
+});
 
-    
-      
-
-
-   expireDate:{
-    type:Date,
-    required:true
-   },
-
-   
-},{timestamps:true,
-   
-},
-)
-
-
-const couponModel=model('Coupon',couponSchema)
+const couponModel = model('Coupon', couponSchema);
 export default couponModel;
