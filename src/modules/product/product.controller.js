06393358caa2abd81da5a9476ba45cae674f0887
FatimaOmair <fatimaemer2002@gmail.com>
2024-06-03 11:02:30 +0300

    create pagination

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -3,6 +3,7 @@ import categoryModel from "../../../db/model/category.model.js";
 import subCategoryModel from "../../../db/model/subCategory.model.js";
 import cloudinary from "../../ults/cloudinary.js";
 import productModel from "../../../db/model/product.model.js";
+import { pagination } from "../../ults/pagination.js";
 
 export const createProduct=async(req,res,next)=>{
 const {name,discount,price,categoryId,subCategoryId}=req.body;
@@ -42,3 +43,54 @@ for(const file of req.files.subImages){
 const product =await productModel.create(req.body)
 return res.status(200).json({message:"success",product})
 }
+
+
+export const getProducts = async (req, res, next) => {
+    try {
+        const { page = 1, limit = 2, sort } = req.query;
+        const { skip, limit: parsedLimit } = pagination(page, limit);
+
+        let queryObject = { ...req.query };
+        const excludeQuery = ['page', 'limit', 'sort','search','fields'];
+
+        excludeQuery.forEach((ele) => {
+            delete queryObject[ele];
+        });
+
+        queryObject = JSON.stringify(queryObject);
+        queryObject = queryObject.replace(/\b(gt|gte|lt|lte|in|nin|eq)\b/g, (match) => `$${match}`);
+        queryObject = JSON.parse(queryObject);
+
+        let productsQuery = productModel.find(queryObject)
+            .skip(skip)
+            .limit(parsedLimit)
+           
+            .populate({
+                path: 'reviews',
+                populate: {
+                    path: 'userId',
+                    select: 'userId -_id'
+                }
+            })
+            .select(req.query.fields);
+
+        if(req.query.searh){
+            productsQuery.find({
+                $or:[
+                    {name:{$regex:req.query.search},
+                     description:{$regex:req.query.search}}
+                ]
+            })
+        }
+        if (sort) {
+            const sortFields = sort.split(',').map(field => field.trim()).join(' ');
+            productsQuery = productsQuery.sort(sortFields);
+        }
+
+        const products = await productsQuery;
+
+        return res.status(200).json({ message: "success", products });
+    } catch (error) {
+        next(error);
+    }
+};
