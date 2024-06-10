3427f7733108ac8b955ab3d0d0a2bd13a1fe64af
FatimaOmair <fatimaemer2002@gmail.com>
2024-06-10 10:22:59 +0300

    add validation to the category

--- src/modules/category/category.router.js
+++ src/modules/category/category.router.js
@@ -4,16 +4,18 @@ import fileUpload, { fileType } from '../../ults/multer.js';
 import { auth, roles } from '../../middleware/auth.js';
 import subCategoryRouter from './../subCategory/subCategory.router.js'
 import { endPoint } from './category.roles.js';
+import { validation } from '../../middleware/validation.js';
+import * as schema from './category.validation.js'
 const router= Router();
 
 
 router.use('/:id/subCategory',subCategoryRouter)
-router.post('/',auth(endPoint.create),fileUpload(fileType.image).single('image'), categoryController.create);
+router.post('/',fileUpload(fileType.image).single('image'),validation(schema.createCategorySchema),auth(endPoint.create), categoryController.create);
 router.get('/',auth(endPoint.get),categoryController.getAll)
 router.get('/active',auth(endPoint.get),categoryController.getActive)
 router.get('/:id',auth(endPoint.get),categoryController.getCategory)
-router.patch('/:id',auth(),fileUpload(fileType.image).single('image'),categoryController.updateCategory)
-router.delete('/:id',auth(endPoint.delete),categoryController.deleteCategory)
+router.patch('/:id',fileUpload(fileType.image).single('image'),validation(schema.updateCategorySchema),auth(),categoryController.updateCategory)
+router.delete('/:id',validation(schema.deleteCategorySchema),auth(endPoint.delete),categoryController.deleteCategory)
 
 router.get('/name/:name',categoryController.getName)
 
