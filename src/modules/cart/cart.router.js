78602e2b6ade481f96be0817151eae3f6af10b17
FatimaOmair <fatimaemer2002@gmail.com>
2024-06-10 10:22:23 +0300

    add validation

--- src/modules/cart/cart.router.js
+++ src/modules/cart/cart.router.js
@@ -2,10 +2,12 @@ import {Router} from 'express'
 import * as cartController from './cart.controller.js'
 import { auth, roles } from '../../middleware/auth.js';
 import { endPoint } from './cart.roles.js';
+import { validation } from '../../middleware/validation.js';
+import * as schema from './cart.validation.js'
 const router= Router();
 
 
-router.get('/',auth(endPoint.create), cartController.getCart);
+router.get('/',validation(schema.createCartSchema),auth(endPoint.create), cartController.getCart);
 router.post('/',auth(endPoint.create), cartController.create);
 router.put('/increase/:productId',auth(endPoint.create), cartController.increase)
 router.put('/decrease/:productId',auth(endPoint.create), cartController.decrease)
